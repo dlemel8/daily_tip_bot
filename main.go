a2f7a41b8a9aa90410cd80b136b2157baac6eab8
daniels <daniels@cybermdx.com>
2020-01-08 23:19:37 +0200

    missing commit

--- main.go
+++ main.go
@@ -1,1 +1,31 @@
-package daily_tip_bot
+package main
+
+import (
+	"errors"
+	"fmt"
+	"github.com/prometheus/common/log"
+	"os"
+
+	"github.com/nlopes/slack"
+)
+
+
+func run() error {
+	token := os.Getenv("SLACK_TOKEN")
+	if token == "" {
+		return errors.New("please set SLACK_TOKEN env var")
+	}
+
+	api := slack.New(token)
+	if _, err := api.AuthTest(); err != nil {
+		return fmt.Errorf("token is not valid: %w", err)
+	}
+
+	return nil
+}
+
+func main() {
+	if err := run(); err != nil {
+		log.Fatal(err)
+	}
+}
