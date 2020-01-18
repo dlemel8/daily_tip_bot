23f1c957794695b5ab892e6a0d4935879976fa05
daniels <daniels@cybermdx.com>
2020-01-18 21:51:04 +0200

    setting port from env var

--- main.go
+++ main.go
@@ -2,12 +2,18 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	log "github.com/sirupsen/logrus"
 	"net/http"
 	"os"
 )
 
 func run() error {
+	port := os.Getenv("PORT")
+	if port == "" {
+		return errors.New("please set PORT env var")
+	}
+
 	signingSecret := os.Getenv("SLACK_SIGNING_SECRET")
 	if signingSecret == "" {
 		return errors.New("please set SLACK_SIGNING_SECRET env var")
@@ -16,7 +22,7 @@ func run() error {
 	http.HandleFunc("/slack", newSlashCommandHandler(signingSecret))
 
 	log.Info("Server listening")
-	return http.ListenAndServe(":8080", nil)
+	return http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
 }
 
 func main() {
