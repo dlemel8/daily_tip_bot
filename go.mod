c843a5ad49a45b19f65ea743086e1c6946ce2340
daniels <daniels@cybermdx.com>
2020-01-26 23:46:20 +0200

    use cobra and vyper to create new subcommand and read env vars

--- go.mod
+++ go.mod
@@ -6,4 +6,6 @@ require (
 	github.com/nlopes/slack v0.6.0
 	github.com/prometheus/common v0.7.0
 	github.com/sirupsen/logrus v1.4.2
+	github.com/spf13/cobra v0.0.5
+	github.com/spf13/viper v1.3.2
 )
--- main.go
+++ main.go
@@ -1,32 +1,54 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 	"net/http"
 	"os"
 )
 
-func run() error {
-	port := os.Getenv("PORT")
-	if port == "" {
-		return errors.New("please set PORT env var")
+var (
+	rootCmd = &cobra.Command{
+		Use: "daily_tip_bot",
 	}
+	webServiceCmd = &cobra.Command{
+		Use: "web_service",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			signingSecret := viper.GetString("slack_signing_secret")
+			http.HandleFunc("/slack", newSlashCommandHandler(signingSecret))
 
-	signingSecret := os.Getenv("SLACK_SIGNING_SECRET")
-	if signingSecret == "" {
-		return errors.New("please set SLACK_SIGNING_SECRET env var")
+			log.Info("Server listening")
+			port := viper.GetInt("port")
+			return http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+		},
 	}
+	scheduledTipsCmd = &cobra.Command{
+		Use: "scheduled_tips",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			botToken := viper.GetString("slack_bot_token")
+			fmt.Printf("bot token len is %d\n", len(botToken))
+			return nil
+		},
+	}
+)
 
-	http.HandleFunc("/slack", newSlashCommandHandler(signingSecret))
-
-	log.Info("Server listening")
-	return http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
+func init() {
+	viper.AutomaticEnv()
+	rootCmd.AddCommand(webServiceCmd)
+	rootCmd.AddCommand(scheduledTipsCmd)
 }
 
 func main() {
-	if err := run(); err != nil {
+	cmd, _, _ := rootCmd.Find(os.Args[1:])
+	if cmd != nil && cmd.Name() == rootCmd.Name() {
+		// set default command
+		args := append([]string{webServiceCmd.Name()}, os.Args[1:]...)
+		rootCmd.SetArgs(args)
+	}
+
+	if err := rootCmd.Execute(); err != nil {
 		log.Fatal(err)
 	}
 }
